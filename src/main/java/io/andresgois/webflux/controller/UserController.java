a64c68432f1e114d82fdc46f0e6be802ba300ee9
Andre Gois <andresousalee@gmail.com>
2024-05-11 14:34:41 -0300

    :feat: define interfaces

---
+++ src/main/java/io/andresgois/webflux/controller/UserController.java
@@ -0,0 +1,25 @@
+package io.andresgois.webflux.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface UserController {
+
+    @PostMapping
+    ResponseEntity<Mono<Void>> save(@RequestBody UserRequest request);
+
+    @GetMapping(value = "/{id}")
+    ResponseEntity<Mono<UserResponse>> find(@PathVariable("id") String id);
+
+    @GetMapping
+    ResponseEntity<Flux<UserResponse>> findall();
+
+    @PatchMapping(value = "/{id}")
+    ResponseEntity<Mono<UserResponse>> update(@PathVariable String id,
+                                              @RequestBody UserRequest request);
+
+    @DeleteMapping(value = "/{id}")
+    ResponseEntity<Mono<Void>> delete(@PathVariable("id") String id);
+}
