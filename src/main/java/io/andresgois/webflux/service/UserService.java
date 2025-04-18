a1e63a91b687d782371ee726957f230e01da7dd9
Andre Gois <andresousalee@gmail.com>
2025-04-18 20:10:24 -0300

    git commit -m ":sparkles: feat: implementação do service de usuário"

---
+++ src/main/java/io/andresgois/webflux/service/UserService.java
@@ -0,0 +1,21 @@
+package io.andresgois.webflux.service;
+
+import io.andresgois.webflux.controller.domain.request.UserRequest;
+import io.andresgois.webflux.domain.User;
+import io.andresgois.webflux.mapper.UserMapper;
+import io.andresgois.webflux.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+	private final UserRepository userRepository;
+	private final UserMapper userMapper;
+
+	public Mono<User> save(final UserRequest request) {
+		return userRepository.save(userMapper.toEntity(request));
+	}
+}
