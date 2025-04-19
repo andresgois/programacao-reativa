812c2d159022df68551175d4e32dc8fdb9d6a4a5
Andre Gois <andresousalee@gmail.com>
2025-04-18 23:45:21 -0300

    git commit -m ":sparkles: feat: adicionando validação para email duplicado"

---
+++ src/main/java/io/andresgois/webflux/controller/exceptions/ControllerExcetionHandle.java
@@ -0,0 +1,58 @@
+package io.andresgois.webflux.controller.exceptions;
+
+
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.support.WebExchangeBindException;
+import reactor.core.publisher.Mono;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class ControllerExcetionHandle {
+
+	@ExceptionHandler(DuplicateKeyException.class)
+	ResponseEntity<Mono<StandardError>> handleDuplicateKeyException(DuplicateKeyException ex,
+																	ServerHttpRequest request
+	) {
+		return ResponseEntity.badRequest()
+				.body(Mono.just(
+						StandardError.builder()
+								.timestamp(LocalDateTime.now())
+								.status(HttpStatus.BAD_REQUEST.value())
+								.error(HttpStatus.BAD_REQUEST.getReasonPhrase())
+								.message(verifyDupKey(ex.getMessage()))
+								.path(request.getPath().toString())
+								.build()
+				));
+	}
+
+	@ExceptionHandler(WebExchangeBindException.class)
+	public ResponseEntity<Mono<ValidationError>> validationError(
+			WebExchangeBindException ex, ServerHttpRequest request
+	) {
+		ValidationError error = new ValidationError(
+				LocalDateTime.now(), request.getPath().toString(),
+				HttpStatus.BAD_REQUEST.value(), "Validation error", "Error on validation attributes"
+		);
+
+		for (FieldError x : ex.getBindingResult().getFieldErrors()) {
+			error.addError(x.getField(), x.getDefaultMessage());
+		}
+
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Mono.just(error));
+	}
+
+	private String verifyDupKey(String message) {
+		if (message.contains("indice_email_unico dup key")) {
+			return "E-mail already registered";
+		}
+		return "Dup key exception";
+	}
+}
+
